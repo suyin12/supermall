c6b7ea5b0aa46e673725d97f448ee915804b590b
sujianhui <sujianhui@beyondsoft.com>
2021-04-19 18:03:36 +0800

    navbar

---
+++ src/components/common/navbar/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="nav-bar">
+    <div class="left">
+      <slot name="left">1</slot>
+    </div>
+    <div class="center">
+      <slot name="center">2</slot>
+    </div>
+    <div class="right">
+      <slot name="right">3</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+  .nav-bar {
+    display: flex;
+    height: 44px;
+    line-height: 44px;
+    text-align: center;
+    box-shadow: 0 1px 1px rgba(100,100,100,.1);
+  }
+
+  .left, .right {
+    width: 60px;
+  }
+
+  .center {
+    background-color: aliceblue;
+    flex: 1;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,15 @@
 <template>
-  <div id="home" class="wrapper">
-
+  <div id="home">
+    <navbar/>
   </div>
 </template>
 
 <script>
+  import Navbar from "components/common/navbar/Navbar";
   export default {
     name: "Home",
     components: {
-
+      Navbar
     },
     data() {
       return {
